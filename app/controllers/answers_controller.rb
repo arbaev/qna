8904b0bf1f172ea379d0a5135beda5978ff46eb5
Tim Arbaev <timbrk@gmail.com>
2019-04-04 17:11:46 +0300

    add answers#new

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+  before_action :question, only: %i[new]
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  private
+
+  def question
+    @question = Question.find(params[:question_id])
+  end
 end
