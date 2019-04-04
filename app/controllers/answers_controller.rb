4fba91b50198ae416d753d111a10df5ffc986250
Tim Arbaev <timbrk@gmail.com>
2019-04-04 17:28:06 +0300

    add answers#create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,27 @@
 class AnswersController < ApplicationController
-  before_action :question, only: %i[new]
+  before_action :question, only: %i[new create]
 
   def new
     @answer = @question.answers.new
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
   end
 
   private
 
   def question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
