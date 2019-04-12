95739240a65c99b529e38d4ea7cbcd96e719b71b
Tim Arbaev <timbrk@gmail.com>
2019-04-12 06:54:32 +0300

    db seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tim = User.create!(email: 'tim@qna.io', password: '123456')
+zeus = User.create!(email: 'zeus@qna.io', password: '123456')
+afina = User.create!(email: 'afina@qna.io', password: '123456')
+
+authors = [tim, zeus, afina]
+
+QUESTIONS_NUMBER = 6
+ANSWERS_NUMBER = 3
+
+QUESTIONS_NUMBER.times do |i|
+  quest = Question.create!(title: "#{Faker::Coffee.blend_name} question",
+                           body: "Coffe from #{Faker::Coffee.origin}. What does it taste like?",
+                           author: authors.sample)
+  ANSWERS_NUMBER.times do |j|
+    Answer.create!(body: "It's a #{Faker::Coffee.notes} taste!",
+                   question: quest,
+                   author: authors.sample)
+  end
+end
