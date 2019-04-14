cfec576166fd07a6005cfa558dce53ebe2cddfaa
Tim Arbaev <timbrk@gmail.com>
2019-04-14 10:58:20 +0300

    mentor: fix feature test create answer

--- spec/features/answers/create_answer_spec.rb
+++ spec/features/answers/create_answer_spec.rb
@@ -7,7 +7,6 @@ feature 'user can create answer to the question', %q{
 
   given(:user) { create(:user) }
   given!(:question) { create(:question, author: user) }
-  given!(:answers) { create_list(:answers_list, 3, question: question, author: user) }
 
   describe 'Authenticated user' do
     background do
@@ -16,21 +15,26 @@ feature 'user can create answer to the question', %q{
     end
 
     scenario 'create a new answer to the question' do
-      fill_in 'Body', with: answers.first.body
+      text = Faker::Number.hexadecimal(10)
+      fill_in 'Body', with: text
       click_on 'Create answer'
+
       expect(page).to have_content 'answer successfully created'
-      expect(page).to have_content answers.first.body
+      expect(page).to have_content text
     end
 
     scenario 'create empty answer' do
       fill_in 'Body', with: nil
       click_on 'Create answer'
+
       expect(page).to have_content "please, enter answer's text"
+      expect(page).to have_content "Body can't be blank"
     end
   end
 
   scenario 'unAuth user cannot create answer' do
     visit question_path(question)
+    fill_in 'Body', with: 'just sample text'
     click_on 'Create answer'
 
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
