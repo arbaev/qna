9a5ad0c9b782f42fb8df75a798453803a3e23961
Tim Arbaev <timbrk@gmail.com>
2019-05-03 11:40:35 +0300

    refactor gist helper to gist service

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Link, type: :model do
 
   INVALID_URLS = %w[http:///foo.com buz http:// http://http:// https://rails. https://rails rails.com https://rails.com\questions].freeze
   it { should_not allow_values(INVALID_URLS).for(:url) }
+
+  context 'test gist links' do
+    let!(:question) { create(:question) }
+    let(:regular_link) { question.links.create!(name: 'Regular link', url: 'http://ya.ru') }
+    let(:gist_link) { question.links.create!(name: 'Gist link', url: 'https://gist.github.com/arbaev/53545d511a35e97da9afef00cf7770ea') }
+
+    it 'gist link is gist?' do
+      expect(gist_link).to be_gist_link
+    end
+
+    it 'regular link is gist?' do
+      expect(regular_link).to_not be_gist_link
+    end
+  end
 end
