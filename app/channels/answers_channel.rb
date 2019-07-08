7e2075b902053c7f17a26c81c88181455e3f58f3
Tim Arbaev <timbrk@gmail.com>
2019-07-08 13:30:22 +0300

    streams names small fix

--- app/channels/answers_channel.rb
+++ app/channels/answers_channel.rb
@@ -1,7 +1,7 @@
 class AnswersChannel < ApplicationCable::Channel
   def follow(data)
     if data['question_id']
-      stream_from "question#{data['question_id'].to_i}:answers"
+      stream_from "question#{data['question_id']}:answers"
     else
       reject
     end
