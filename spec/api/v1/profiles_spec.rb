2331c017ad8535a9b69443e463cc10f35384ffac
Tim Arbaev <timbrk@gmail.com>
2019-07-28 09:24:20 +0300

    shared examples authorization

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -5,27 +5,18 @@ describe 'Profiles API', type: :request do
                      "ACCEPT" => 'application/json' } }
 
   describe 'GET /api/v1/profiles/me' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles/me', headers: headers
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles/me', params: { access_token: '1234' }, headers: headers
-        expect(response.status).to eq 401
-      end
+    let(:api_path) { '/api/v1/profiles/me' }
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
     end
 
     context 'authorized' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get '/api/v1/profiles/me', params: { access_token: access_token.token }, headers: headers }
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
-      it 'returns 200 status' do
-        expect(response).to be_successful
-      end
+      it_behaves_like 'Request successful'
 
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
@@ -42,16 +33,9 @@ describe 'Profiles API', type: :request do
   end
 
   describe 'GET /api/v1/profiles' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles', headers: headers
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles', params: { access_token: '1234' }, headers: headers
-        expect(response.status).to eq 401
-      end
+    let(:api_path) { '/api/v1/profiles' }
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
     end
 
     context 'authorized' do
@@ -61,9 +45,7 @@ describe 'Profiles API', type: :request do
 
       before { get '/api/v1/profiles', params: { access_token: access_token.token }, headers: headers }
 
-      it 'returns 200 status' do
-        expect(response).to be_successful
-      end
+      it_behaves_like 'Request successful'
 
       it 'return all users but me' do
         expect(json['users'].size).to eq users.size - 1
