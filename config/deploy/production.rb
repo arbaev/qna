ced47eda04147f40656815f3e233916c08d26290
Tim Arbaev <timbrk@gmail.com>
2019-08-09 22:40:20 +0300

    cap configs

---
+++ config/deploy/production.rb
@@ -0,0 +1,44 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+server "167.71.65.162", user: "deployer", roles: %w{app db web}, primary: true
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
+set :rail_env, :production
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+ set :ssh_options, {
+   keys: %w(/Users/Tim/.ssh/id_rsa),
+   forward_agent: true,
+   auth_methods: %w(publickey password),
+   port: 2222
+ }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
